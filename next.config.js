155b14eff9cfcc20d94007c98a8cd3a554a14fce
dimzzYT21 <gtavmodding21@gmail.com>
2026-09-08 16:29:22 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
+
 /** @type {import('next').NextConfig} */
+import { redirect } from 'next/navigation'
+
 const nextConfig = {
   images: {
     domains: ['images.unsplash.com', 'via.placeholder.com'],
   },
+}
+export default function RootPage() {
+  // Langsung alihkan pengguna ke halaman aplikasi/kasir
+  redirect('/admin/pos')
 }
 
 module.exports = nextConfig
